57cb174079133c9e0681ef69fea4be2b6df028a1
dhieisonMS <jhasonx@gmail.com>
2022-06-26 23:30:45 -0300

    .

--- JogoRPG/DUNGEON_DIO copy/Program.cs
+++ JogoRPG/DUNGEON_DIO copy/Program.cs
@@ -15,7 +15,7 @@ namespace DUNGEON_DIO
             Console.WriteLine(batman);
 
             Console.WriteLine(batman.Atack());
-            Console.WriteLine(teste.Atack(10000));
+            Console.WriteLine(teste.Atack(1011000));
             Console.WriteLine(ellen.Atack(110));
         }
     }
